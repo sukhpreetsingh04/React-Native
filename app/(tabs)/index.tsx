66e2b581fcce55f1130e777cc24109ee30764dca
sukhpreetsing04 <sukhpreetsingh19289@gmail.com>
2025-09-18 17:47:36 +0530

    HelloWorld App

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, StyleSheet} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={styles.safeArea} edges={['top', 'bottom']}>
-      <View style={styles.header}>
-        <Text style={styles.title}>Android App</Text>
-      </View>
+    <View style={styles.container}>
+        <Text>Hello from Android</Text>
       <StatusBar style="auto" />
-    </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
+  container: {
     flex: 1,
-    backgroundColor: '#f2f2f2',
-    paddingHorizontal: 20,
-    justifyContent: 'center',
-  },
-  header: {
-    marginBottom: 40,
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-  body: {
-    alignItems: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
