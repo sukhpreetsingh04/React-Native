33615d33d73966c0be9f0cdf5e2b8f13fe2614cd
sukhpreetsing04 <sukhpreetsingh19289@gmail.com>
2025-10-07 11:14:16 +0530

    edits in index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View, StyleSheet} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-        <Text>Hello from Android</Text>
+      <Text>Hello from Android</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,8 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#3a3e53ff",
     alignItems: "center",
     justifyContent: "center",
-  },
+  }
 });
